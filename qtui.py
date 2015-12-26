e3e6ebddbf31597a973eaf803986ed3a9de25e1d
Bruno Bonfils <asyd@asyd.net>
2015-12-26 05:28:15 +0100

    Switch to QT5

--- qtui.py
+++ qtui.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt4 import QtGui, QtCore, uic
 import logging
 import zmq
+import json
+from PyQt5 import QtCore, QtWidgets, QtGui, uic
 
 
 class BoilerStatusListener(QtCore.QObject):
@@ -23,47 +24,54 @@ class BoilerStatusListener(QtCore.QObject):
     def loop(self):
         while self.running:
             string = self.socket.recv_string()
-            self.message.emit("test")
+            logging.debug("Receive message %s from 0mq" % string)
+            # Remove topic
+            string = string.replace('boiler ', '', 1)
+            # self.message.emit(string)
 
 
-class ThermostatApp(QtGui.QMainWindow):
+class ThermostatApp(QtWidgets.QMainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
         self.thread = QtCore.QThread()
+        self.test = False
 
         self.ui = uic.loadUi('thermostat.ui')
         self.ui.show()
 
-        self.connect(self.ui.forceButton, QtCore.SIGNAL("clicked()"), self.boiler_force)
-        self.timer = QtCore.QTimer()
-        self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.check_boiler)
-        self.timer.start(1000)
-
-        self.listener = BoilerStatusListener()
-        self.listener.moveToThread(self.thread)
-        self.thread.started.connect(self.listener.loop)
-        self.listener.message.connect(self.signal_received)
-        self.signal_received("test")
-
-        QtCore.QTimer.singleShot(0, self.thread.start)
-        # self.ui.forceButton.setEnabled(True)
+        self.ui.forceButton.clicked.connect(self.boiler_force)
+        # self.connect(self.ui.forceButton, QtCore.SIGNAL("clicked()"), self.boiler_force)
+        #
+        # self.listener = BoilerStatusListener()
+        # self.listener.moveToThread(self.thread)
+        # self.thread.started.connect(self.listener.loop)
+        # self.listener.message.connect(self.signal_received)
+        #
+        # QtCore.QTimer.singleShot(0, self.thread.start)
+        # # Disable the force button by default
+        # self.ui.forceButton.setEnabled(False)
 
     def boiler_force(self):
         logging.debug("Force boiler to on")
-
-    def check_boiler(self):
-        logging.debug("Check if boiler can be enabled")
 
     def timer(self):
         logging.debug("timer called")
 
     def signal_received(self, message):
-        logging.debug("Message: %s" % message)
+        logging.debug("signal_received: %s" % message)
+        # try:
+        boiler = json.loads(message)
+        if boiler['status'] == 0:
+            self.ui.forceButton.setEnabled(True)
+            self.ui.forceButton.setFocus()
+            self.ui.forceButton.repaint()
+        # except Exception as e:
+        #     logging.critical("Exception in signal_received: %s" % e.__str__())
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     win = ThermostatApp()
     sys.exit(app.exec_())
 
